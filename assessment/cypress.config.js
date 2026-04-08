ea2976ddf00edb4bae930b4aa41c6fec3a2408e3
CrioUser <crio-user@criodo.com>
2026-04-08 21:39:35 +0530

    “ME_FE_BUILDOUT_XSTATES_MODULE_ONE”

--- assessment/cypress.config.js
+++ assessment/cypress.config.js
@@ -2,12 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // Add the Vercel URL with the share token here
-    baseUrl: "https://cli-flino-s-projects.vercel.app?_vercel_share=eLBzxj16q3KIYJEL9HjObFxX8JWALDaA",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // Optional: This prevents Cypress from failing if it hits a stray 401/403
-    failOnStatusCode: false 
   },
 });
